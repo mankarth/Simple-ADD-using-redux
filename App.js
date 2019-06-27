dfcfaf5e01a9c567b60ff93e004a9ce57fd76bc2
mankarth <51682657+mankarth@users.noreply.github.com>
2019-06-27 09:42:38 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+//import { createStore } from 'redux';
+
+const { createStore } = require('redux');
+
+const initState = {
+    age: 12
+};
+
+const myReducer = (state = initState, action) => {
+    const newState = { ...state };
+
+    if (action.type === 'Increment'){
+        newState.age += 1;
+    }
+    return newState;
+}
+
+const store = createStore(myReducer);
+
+store.dispatch({ type: 'Increment' });
+
+console.log(store.getState());
